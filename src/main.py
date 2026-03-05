2b6be3de0b32ffc2a8e44321a2a9f30605cb7268
vvlaads <vvlaads@gmail.com>
2026-03-05 22:27:01 +0300

    Added templates of methods and some realizations

--- src/main.py
+++ src/main.py
@@ -1,1 +1,18 @@
-print("Hello")
+import random
+
+from methods import *
+
+
+def function(x):
+    """Функция f(x) = x^2"""
+    return x ** 2
+
+
+a = 2
+b = 5
+integral = 39
+
+rng = random.Random(42)
+print(f"Значение интеграла: {integral}")
+print(f"Простой метод Монте-Карло: {monte_carlo_function(rng, a, b, function)}")
+print(f"Метод Монте-Карло со стратификацией: {monte_carlo_function_with_stratification(rng, a, b, function)}")
---
+++ src/methods.py
@@ -0,0 +1,37 @@
+def monte_carlo_function(rng, a, b, f):
+    """Простое интегрирование методом Монте-Карло"""
+    xi = rng.uniform(a, b)
+    return (b - a) * f(xi)
+
+
+def monte_carlo_function_with_stratification(rng, a, b, f, step=0.5):
+    """Интегрирование методом Монте-Карло со стратификацией"""
+    borders = [a]
+
+    while borders[-1] + step < b:
+        borders.append(borders[-1] + step)
+
+    borders.append(b)
+
+    s = 0
+    for i in range(len(borders) - 1):
+        start = borders[i]
+        end = borders[i + 1]
+        s += monte_carlo_function(rng, start, end, f)
+
+    return s
+
+
+def monte_carlo_function_with_sampling_by_significance(rng, a, b, f):
+    """Интегрирование методом Монте-Карло с выборкой по значимости"""
+    return monte_carlo_function(rng, a, b, f)
+
+
+def monte_carlo_function_with_multiple_sampling_by_significance(rng, a, b, f):
+    """Интегрирование методом Монте-Карло с многократной выборкой по значимости"""
+    return monte_carlo_function(rng, a, b, f)
+
+
+def monte_carlo_function_with_russian_roulette(rng, a, b, f):
+    """Интегрирование методом Монте-Карло с использованием русской рулетки"""
+    return monte_carlo_function(rng, a, b, f)
